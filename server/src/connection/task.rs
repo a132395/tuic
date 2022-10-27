de3bad24788c9ad701797c1484e145dff50e87d6
a132395 <67187730+a132395@users.noreply.github.com>
2022-10-27 16:51:24 +0800

    Update task.rs

--- server/src/connection/task.rs
+++ server/src/connection/task.rs
@@ -57,7 +57,7 @@ pub async fn connect(
         let ipv6_tx = Arc::clone(&arc_tx);
         if ipv6_addrs.len() > 0 {
                 tokio::spawn(async move {
-                ipv6_tx.send(tcp_connect(ipv6_addrs).await?).await?.ok();
+                ipv6_tx.send(Ok(tcp_connect(ipv6_addrs).await?)).await?;
             });
         }
 
@@ -65,7 +65,7 @@ pub async fn connect(
 
         if ipv4_addrs.len() > 0 {
                 tokio::spawn(async move {
-                ipv4_tx.send(tcp_connect(ipv4_addrs).await?).await?.ok();
+                ipv4_tx.send(Ok(tcp_connect(ipv4_addrs).await?)).await?;
             });
         }
 
